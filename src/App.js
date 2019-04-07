051c1471a4995871e56cc0a3d94a4098c68f7f6c
Paulina Oster <paulina.oster@gmail.com>
2019-04-07 22:13:09 +0200

    added 2 buttons/functions for +5 and -5, moved styles to styles.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RemoveIcon from '@material-ui/icons/Remove';
 import Fab from '@material-ui/core/Fab';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import styles from './styles';
 
 class App extends React.Component {
     state = {
@@ -11,19 +12,17 @@ class App extends React.Component {
     }
 
     inc = () => this.setState({ number: this.state.number + 1 })
+    inc5 = () => this.setState({ number: this.state.number + 5 })
 
     dec = () => this.setState({ number: this.state.number - 1 })
+    dec5 = () => this.setState({ number: this.state.number - 5 })
 
     reset = () => this.setState({ number: this.props.number})
 
     render() {
         return (
             <Paper
-                style={{
-                    width: '50%',
-                    margin: '10px auto',
-                    backgroundColor: 'lightblue'
-                }}
+                style={styles.paper}
             >
                 <Typography
                     align="center"
@@ -35,18 +34,35 @@ class App extends React.Component {
                     <p>Counter</p>
                     <p>{this.state.number}</p>
                     <Fab
+                        style={styles.button}
                         color="primary"
                         onClick={this.inc}
                     >
                         <AddIcon />
                     </Fab>
                     <Fab
+                        style={styles.button}
                         color="secondary"
                         onClick={this.dec}
                     >
                         <RemoveIcon />
                     </Fab>
                     <Fab
+                        style={styles.button}
+                        color="primary"
+                        onClick={this.inc5}
+                    >
+                        +5
+                    </Fab>
+                    <Fab
+                        style={styles.button}
+                        color="secondary"
+                        onClick={this.dec5}
+                    >
+                        -5
+                    </Fab>
+                    <Fab
+                        style={styles.button}
                         onClick={this.reset}
                         variant="extended"
                     >
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+const styles = {
+    paper: {
+        width: '50%',
+        margin: '10px auto',
+        backgroundColor: 'lightblue'
+    },
+    button: {
+        margin: '20px',
+    }
+}
+export default styles;
